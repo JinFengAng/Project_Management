07f7189d33958f5cb61da5f2cf795a7b75a79938
JinFengAng <angjfwork@gmail.com>
2022-02-08 10:36:19 +0800

    add index

--- logOut.php
+++ logOut.php
@@ -3,5 +3,5 @@ require('config.php');
 session_start();
 session_unset();
 echo "<script>alert('You have logout successfully!')</script>";
-echo "<script> window.location='loginAdmin.php'</script>";
+echo "<script> window.location='index.php'</script>";
 ?>
